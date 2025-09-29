98f3eca4fec95254a057327822a236265f07f306
alamkhan20042004 <ak5991079@gmail.com>
2025-09-29 05:51:36 -0700

    qgqin back

--- app/layout.js
+++ app/layout.js
@@ -30,9 +30,9 @@ export default function RootLayout({ children }) {
 
   {/* <script type="text/javascript" src="https://hotbmifogi.today/process.js?id=1483243874&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script>   inpush ads */}
   
-  {/* <script type="text/javascript" src="https://hotbmifogi.today/process.js?id=1483243874&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script> */}
-  
   <script type="text/javascript" src="https://hotbmifogi.today/process.js?id=1483243874&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script>
+  
+  {/* <script type="text/javascript" src="https://hotbmifogi.today/process.js?id=1483243874&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script> */}
 
 
 
