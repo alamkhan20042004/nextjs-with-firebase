623e43e9875e69f82ae6212573c902a0130accf1
alamkhan20042004 <ak5991079@gmail.com>
2025-10-14 01:57:19 -0700

    adoperator

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,17 @@ export default function RootLayout({ children }) {
 
           {/* <script type="text/javascript" src="https://hotbmifogi.today/process.js?id=1483243874&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script> */}
           
-  <script type="text/javascript" src="https://hotbnupore.cc/process.js?id=1498866775&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script>
+  {/* <script type="text/javascript" src="https://hotbnupore.cc/process.js?id=1498866775&p1=sub1&p2=sub2&p3=sub3&p4=sub4" async> </script> */}
+
+
+
+  {/* ======== adoperator =============== */}
+    <script data-cfasync='false' src='//wwr.giriudog.com/?tag=793e9f46'></script>
+
+
+
+
+
   {/* ================= META TAGS ================= */}
 
   
